200187b3b4ea5ec0088db64e61ef5d9220f8a22d
juanjosegr <juanjosegr9@gmail.com>
2023-11-20 20:28:18 +0100

    añadido todo

---
+++ app/src/main/java/com/example/cartas/Screens/Baraja.kt
@@ -0,0 +1,66 @@
+package com.example.cartas.Screens
+
+
+enum class Palos {
+    CORAZONES, DIAMANTES, TREBOLES, PICAS
+}
+
+enum class Naipes {
+    AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, OCHO, NUEVE, DIEZ, JOTA, REINA, REY
+
+}
+
+class Baraja {
+
+    companion object {
+        var listaCartas = arrayListOf<Carta>()
+        fun crearBaraja() {
+            var idDrawable = 1
+            for (palo in Palos.values()) {
+                for (numero in Naipes.values()) {
+                    val cartaNueva =
+                        Carta(numero, palo, numero.ordinal + 1, numero.ordinal + 1, idDrawable)
+                    if (numero.name == "AS") cartaNueva.puntosMax = 11
+                    listaCartas.add(cartaNueva)
+                    idDrawable++
+                }
+            }
+        }
+
+        fun barajar() {
+            listaCartas.shuffle()
+        }
+
+        fun dameCarta():Carta {
+            return listaCartas.removeAt(listaCartas.size - 1)
+        }
+
+        fun obtenerNombreRecurso(carta: Carta): String {
+            val paloString = when (carta.palo) {
+                Palos.CORAZONES -> "c"
+                Palos.DIAMANTES -> "d"
+                Palos.TREBOLES -> "t"
+                Palos.PICAS -> "n" // Nota: "n" es para picas (corazón negro)
+            }
+
+            val numeroString = when (carta.nombre) {
+                Naipes.AS -> "1"
+                Naipes.DOS -> "2"
+                Naipes.TRES -> "3"
+                Naipes.CUATRO -> "4"
+                Naipes.CINCO -> "5"
+                Naipes.SEIS -> "6"
+                Naipes.SIETE -> "7"
+                Naipes.OCHO -> "8"
+                Naipes.NUEVE -> "9"
+                Naipes.DIEZ -> "10"
+                Naipes.JOTA -> "11"
+                Naipes.REINA -> "12"
+                Naipes.REY -> "13"
+            }
+
+            return "$paloString$numeroString"
+        }
+
+    }
+}
